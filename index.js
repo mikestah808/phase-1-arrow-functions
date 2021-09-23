44ede2f00a70784f6cb37a700bd942a32ab99743
Michael Stafford <michaelstafford808@gmail.com>
2021-09-22 16:52:48 -1000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = function() {
+    // console.log(typeof(a))
+    return 2000 / 100 ;
+    
+};
+
+// console.log(divide(2000 , 100));
+
+// has an arrow function called square
+// square arrow function takes one parameter and multiplies it times itself
+
+let square = x => x * 2;
+
+// has an arrow function called add
+// add arrow function takes two parameters and adds them together
+
+let add = (a, b) => a + b;
+
+
+
